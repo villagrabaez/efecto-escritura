bc03fe2da320d394a658b39899229069fbfe738b
Bernardino Villagra <bernardinovillagra@hotmail.com>
2020-04-05 14:01:38 -0400

    Efecto escritura finalizada

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const typed = new Typed('.typed', {
+  strings: [
+    'Gato',
+    'Perro',
+    'Conejo',
+    'Pez'
+  ],
+
+  // stringsElement: '#cadenas-texto',
+  // smartBackspace: true,
+  typeSpeed: 75,
+  startDelay: 300,
+  backDelay:1500,
+  backSpeed: 75,
+  loop: true,
+  loopCount: false,
+  cursorChar: '|',
+  showCursor: true,
+  shuffle: false,
+  contentType: 'html'
+});
